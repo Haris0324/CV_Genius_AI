638af9be9aaaab81598e6e79d5e2b759c3bbb8ad
Haris0324 <harislaeeq2403@gmail.com>
2026-04-18 22:15:59 +0500

    solving the stripe error and finalizing the app

--- app/api/stripe/checkout/route.js
+++ app/api/stripe/checkout/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { getServerSession } from 'next-auth/react';
+import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../../auth/[...nextauth]/route';
 import { stripe } from '@/lib/stripe';
 import dbConnect from '@/lib/mongodb';
